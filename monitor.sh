4dc615d05c4731d46218a61ac9e8cf3a843182ec
hosewq <hosewq@postech.ac.kr>
2019-03-05 05:34:03 +0000

    add Physical Machine Name for multi-PM

--- monitor.sh
+++ monitor.sh
@@ -18,20 +18,20 @@ get_unixtime(){
 
 push_cpu_data(){
         local cpu_total=$( cat $1 | jq '.cpu.usage.total' )
-	local cpu_total_body="cpu_total,container=$3 value=$cpu_total $2"
+	local cpu_total_body="cpu_total,PM=$PM,container=$3 value=$cpu_total $2"
 	curl -i -XPOST 'http://141.223.82.62:8086/write?db=testdb' --data-binary "$cpu_total_body"
 
 	local per_cpu_length=$( cat $1 | jq '.cpu.usage.per_cpu_usage' | jq length )        
 	for ((i=0;i<$per_cpu_length;i++)); do
 		local per_cpu=$( cat $1 | jq ".cpu.usage.per_cpu_usage[$i]" )
-		local per_cpu_body="per_cpu,container=$3,cpu_index=$i value=$per_cpu $2"
+		local per_cpu_body="per_cpu,PM=$PM,container=$3,cpu_index=$i value=$per_cpu $2"
 		curl -i -XPOST 'http://141.223.82.62:8086/write?db=testdb' --data-binary "$per_cpu_body"
 	done
 
 	local user_usage=$( cat $1 | jq '.cpu.usage.user' )
         local system_usage=$( cat $1 | jq '.cpu.usage.system' )
-        local user_usage_body="user_usage,container=$3 value=$user_usage $2"
-        local system_usage_body="system_usage,container=$3 value=$system_usage $2"
+        local user_usage_body="user_usage,PM=$PM,container=$3 value=$user_usage $2"
+        local system_usage_body="system_usage,PM=$PM,container=$3 value=$system_usage $2"
         curl -i -XPOST 'http://141.223.82.62:8086/write?db=testdb' --data-binary "$user_usage_body"
         curl -i -XPOST 'http://141.223.82.62:8086/write?db=testdb' --data-binary "$system_usage_body"
 }
@@ -42,9 +42,9 @@ push_mem_data(){
 	local mem_workingset=$( cat $1 | jq '.memory.working_set' )
 	#mem_limit
 	
-        local mem_usage_body="mem_usage,container=$3 value=$mem_usage $2"
-        local mem_max_usage_body="mem_max_usage,container=$3 value=$mem_max_usage $2"
-	local mem_workingset_body="mem_workingset,container=$3 value=$mem_workingset $2"
+        local mem_usage_body="mem_usage,PM=$PM,container=$3 value=$mem_usage $2"
+        local mem_max_usage_body="mem_max_usage,PM=$PM,container=$3 value=$mem_max_usage $2"
+	local mem_workingset_body="mem_workingset,PM=$PM,container=$3 value=$mem_workingset $2"
 
         curl -i -XPOST 'http://141.223.82.62:8086/write?db=testdb' --data-binary "$mem_usage_body"
         curl -i -XPOST 'http://141.223.82.62:8086/write?db=testdb' --data-binary "$mem_max_usage_body"	
@@ -60,9 +60,9 @@ push_io_data(){
 		local io_write=$( cat $1 | jq ".diskio[$i].stats.Write" )
 		local io_total=$( cat $1 | jq ".diskio[$i].stats.Total" )
 
-		local io_read_body="io_read,container=$3,device=$io_device value=$io_read $2"
-                local io_write_body="io_write,container=$3,device=$io_device value=$io_write $2"
-		local io_total_body="io_write,container=$3,device=$io_device value=$io_total $2"
+		local io_read_body="io_read,PM=$PM,container=$3,device=$io_device value=$io_read $2"
+                local io_write_body="io_write,PM=$PM,container=$3,device=$io_device value=$io_write $2"
+		local io_total_body="io_write,PM=$PM,container=$3,device=$io_device value=$io_total $2"
 
                 curl -i -XPOST 'http://141.223.82.62:8086/write?db=testdb' --data-binary "$io_read_body"
                 curl -i -XPOST 'http://141.223.82.62:8086/write?db=testdb' --data-binary "$io_write_body"
@@ -79,8 +79,8 @@ push_fs_data(){
         	local fs_capacity=$( cat $1 | jq ".filesystem[$i].capacity" )
         	local fs_usage=$( cat $1 | jq ".filesystem[$i].usage" )
 
-		local fs_capacity_body="fs_capacity,container=$3,device=$device value=$fs_capacity $2"
-        	local fs_usage_body="fs_usage,container=$3,device=$device value=$fs_usage $2"
+		local fs_capacity_body="fs_capacity,PM=$PM,container=$3,device=$device value=$fs_capacity $2"
+        	local fs_usage_body="fs_usage,PM=$PM,container=$3,device=$device value=$fs_usage $2"
 		
         	curl -i -XPOST 'http://141.223.82.62:8086/write?db=testdb' --data-binary "$fs_capacity_body"
         	curl -i -XPOST 'http://141.223.82.62:8086/write?db=testdb' --data-binary "$fs_usage_body"
@@ -103,14 +103,14 @@ push_network_data(){
 		local tx_errors=$( cat $1 | jq ".network[$i].tx_errors" )
 		local tx_dropped=$( cat $1 | jq ".network[$i].tx_dropped" )
 
-		local rx_bytes_body="rx_bytes,container=$3,interface=$iface value=$rx_bytes $2"
-        	local rx_packets_body="rx_packets,container=$3,interface=$iface value=$rx_packets $2"
-        	local rx_errors_body="rx_errors,container=$3,interface=$iface value=$rx_errors $2"
-        	local rx_dropped_body="rx_dropped,container=$3,interface=$iface value=$rx_dropped $2"
-        	local tx_bytes_body="tx_bytes,container=$3,interface=$iface value=$tx_bytes $2"
-        	local tx_packets_body="tx_packets,container=$3,interface=$iface value=$tx_packets $2"
-        	local tx_errors_body="tx_errors,container=$3,interface=$iface value=$tx_errors $2"
-        	local tx_dropped_body="tx_dropped,container=$3,interface=$iface value=$tx_dropped $2"
+		local rx_bytes_body="rx_bytes,PM=$PM,container=$3,interface=$iface value=$rx_bytes $2"
+        	local rx_packets_body="rx_packets,PM=$PM,container=$3,interface=$iface value=$rx_packets $2"
+        	local rx_errors_body="rx_errors,PM=$PM,container=$3,interface=$iface value=$rx_errors $2"
+        	local rx_dropped_body="rx_dropped,PM=$PM,container=$3,interface=$iface value=$rx_dropped $2"
+        	local tx_bytes_body="tx_bytes,PM=$PM,container=$3,interface=$iface value=$tx_bytes $2"
+        	local tx_packets_body="tx_packets,PM=$PM,container=$3,interface=$iface value=$tx_packets $2"
+        	local tx_errors_body="tx_errors,PM=$PM,container=$3,interface=$iface value=$tx_errors $2"
+        	local tx_dropped_body="tx_dropped,PM=$PM,container=$3,interface=$iface value=$tx_dropped $2"
 
 		curl -i -XPOST 'http://141.223.82.62:8086/write?db=testdb' --data-binary "$rx_bytes_body"
 	        curl -i -XPOST 'http://141.223.82.62:8086/write?db=testdb' --data-binary "$rx_packets_body"
@@ -137,6 +137,8 @@ push_data(){
 tmp_cp_stamp=0
 tmp_dp_stamp=0
 tmp_traffic_stamp=0
+
+PM="dpnm_server"
 
 for ((;;))
 do
