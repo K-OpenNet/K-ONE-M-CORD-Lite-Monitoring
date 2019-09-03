358a5af957b61d445ae156532da88d3f9fdc9d9d
Jibum Hong <hosewq@gmail.com>
2019-09-03 11:20:47 +0000

    setting grafana

---
+++ set_grafana.sh
@@ -0,0 +1,2 @@
+docker run -d --name grafana -h grafana -e GF_SECURITY_ADMIN_PASSWORD=pass -p 3000:3000 --link prometheus:prometheus --link prometheus-cadvisor:prometheus-cadvisor grafana/grafana:4.2.0
+
