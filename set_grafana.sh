5b7ed0268c7a6d54d0bd59a6779320efba75f4d8
hosewq <hosewq@gmail.com>
2020-04-06 12:41:25 +0900

    update

--- set_grafana.sh
+++
@@ -1,2 +0,0 @@
-docker run -d --name grafana -h grafana -e GF_SECURITY_ADMIN_PASSWORD=pass -p 3000:3000 --link prometheus:prometheus --link prometheus-cadvisor:prometheus-cadvisor grafana/grafana:4.4.3
-
