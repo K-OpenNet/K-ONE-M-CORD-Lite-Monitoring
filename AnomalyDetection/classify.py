393c926c4089663c3f9db38145d5b65f6a423476
hosewq <hosewq@gmail.com>
2020-04-23 22:28:56 +0900

    update

--- AnomalyDetection/classify.py
+++ AnomalyDetection/classify.py
@@ -1,6 +1,57 @@
+from time import time
+from time import sleep
+import requests
 import subprocess
+import datetime
 
-if __name__ == '__main__':
-    runcommand = "java -cp .:h2o-genmodel.jar main 0 2 1 3 0"
-    result = subprocess.check_output(runcommand.split())
-    print(result)
+instance1_ip="141.223.82.76"
+
+while(True):
+	for cnt in range (0,362):
+		sleep(5)
+
+		now = int(time())
+		#print(now)
+
+		#get cp's cpu usage
+		cp_cpu_query = (
+	    	('query', '100 - avg(collectd_cpu_percent{cpu=~"8|9|10|11",exported_instance="cp",instance="141.223.82.76:9103",job="collectd",type="idle"})'),
+	    	('start', now-5),
+	    	('end', now),
+	    	('step', '5'),
+		)
+
+		dp_cpu_query = (
+	    	('query', '100 - avg(collectd_cpu_percent{cpu=~"0|1|2|3|4|5|6|7",exported_instance="dp",instance="141.223.82.76:9103",job="collectd",type="idle"})'),
+	    	('start', now-5),
+	    	('end', now),
+	    	('step', '5'),
+		)
+
+		cp_cpu_resp = requests.get('http://localhost:39090/api/v1/query_range', params=cp_cpu_query)
+		cp_cpu_data = cp_cpu_resp.json()
+		cp_cpu_value = cp_cpu_data["data"]["result"][0]["values"][1][1]
+		#print(cp_cpu_value)
+
+
+		dp_cpu_resp = requests.get('http://localhost:39090/api/v1/query_range', params=dp_cpu_query)
+		dp_cpu_data = dp_cpu_resp.json()
+		dp_cpu_value =  dp_cpu_data["data"]["result"][0]["values"][1][1]
+		#print(dp_cpu_value)
+
+		if __name__ == '__main__':
+			print(datetime.datetime.now())
+			runcommand_cp = "java -cp .:h2o-genmodel.jar main "+str(cnt)+" "+str(cp_cpu_value)
+			cp_result = subprocess.check_output(runcommand_cp.split())
+			cp_status = cp_result.decode('utf-8').split("\n")[0]
+			print("CP - "+cp_status)
+			if cp_status == "Abnormal":
+				print(" - "+instance1_ip)
+
+			runcommand_dp = "java -cp .:h2o-genmodel.jar main "+str(cnt)+" "+str(dp_cpu_value)
+			dp_result = subprocess.check_output(runcommand_dp.split())
+			dp_status = dp_result.decode('utf-8').split("\n")[0]
+			print("DP - "+dp_status)
+			if dp_status == "Abnormal":
+				print(" - "+instance1_ip)
+			print()
--- AnomalyDetection/main.java
+++ AnomalyDetection/main.java
@@ -6,28 +6,35 @@ import hex.genmodel.MojoModel;
 
 public class main {
   public static void main(String[] args) throws Exception {
-    String modelfile = "StackedEnsemble_BestOfFamily_AutoML_20190703_181005.zip";
+    String modelfile = "GBM_362.zip";
     // String modelfile = "XGBoost_1_AutoML_20190703_181005.zip";
     EasyPredictModelWrapper model = new EasyPredictModelWrapper(MojoModel.load(modelfile));
 
     RowData row = new RowData();
-    row.put("national_inv", args[0]);
-    row.put("forecast_3_month", args[1]);
-    row.put("sales_1_month", args[2]);
-    row.put("sales_9_month", args[3]);
-    row.put("local_bo_qty", args[4]);
+    row.put("Time_In_a_Day", args[0]);
+    row.put("Value", args[1]);
+    //row.put("sales_1_month", args[2]);
+    //row.put("sales_9_month", args[3]);
+    //row.put("local_bo_qty", args[4]);
 
-    System.out.println("Row: " + row);
+    //System.out.println("Row: " + row);
 
     BinomialModelPrediction p = model.predictBinomial(row);
-    System.out.println("Backorder: " + p.label);
-    System.out.print("Class probabilities: ");
-    for (int i = 0; i < p.classProbabilities.length; i++) {
-      if (i > 0) {
-    System.out.print(",");
-      }
-      System.out.print(p.classProbabilities[i]);
-    }
-    System.out.println("");
+    if (p.label.equals("0")){
+	System.out.println("Normal");}
+    else{
+	System.out.println("Abnormal");
+	System.out.print("Check the CPU,mem usage in compute node");
+	}
+	
+    //System.out.println("Label: " + p.label);
+    //System.out.print("Class probabilities: ");
+    //for (int i = 0; i < p.classProbabilities.length; i++) {
+    //  if (i > 0) {
+    //System.out.print(",");
+    //  }
+    //  System.out.print(p.classProbabilities[i]);
+    //}
+    //System.out.println("");
   }
 }
